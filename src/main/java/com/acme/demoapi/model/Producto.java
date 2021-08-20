6634d648976fe07c033fbf1b174895ec6bc3174e
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-08-20 11:58:03 -0500

    first commit

---
+++ src/main/java/com/acme/demoapi/model/Producto.java
@@ -0,0 +1,23 @@
+package com.acme.demoapi.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Producto {
+
+private String id;
+private String name;
+private String categoria;
+private String precio;
+private String cantidad;
+
+
+
+    
+}
